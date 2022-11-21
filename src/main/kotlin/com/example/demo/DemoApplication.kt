4df77c0035b161dd71e8ce824f6ac1395e49dfde
BeckMikkelsen <nikolajbeckmikkelsen@outlook.com>
2022-11-21 10:36:15 +0100

    init

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,41 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.JdbcTemplate
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun save(message: Message){
+        db.update("insert into messages values ( ?, ? )",
+            message.id, message.text)
+    }
+}
+
+@RestController
+class MessageController {
+    @GetMapping
+    fun index() = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
+
+
+data class Message(val id: String?, val text: String)
